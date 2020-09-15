540f5d90eb2f45e80746b556888c3e5677736c9e
SokolK <47599824+SokolK@users.noreply.github.com>
2020-09-15 12:11:28 +0200

    posts added

---
+++ src/components/add_post/add-post.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+
+class AddPost extends Component {
+    state = {
+        UserName: null,
+        Content: null,
+        Title: null
+    }
+    Update = (e) => {
+        this.setState({
+            [e.target.id]: e.target.value
+        })
+    }
+    Sbmit = (e) => {
+        e.preventDefault();
+        this.props.addPost(this.state)
+    }
+    render() {
+        return (
+            <div className="AddPost">
+                <label htmlFor="UserName">UserName: </label>
+                <textarea id="UserName" onChange={this.Update} />
+                <label htmlFor="UserName">Content: </label>
+                <textarea id="Content" onChange={this.Update} />
+                <label htmlFor="UserName">Title: </label>
+                <textarea id="Title" onChange={this.Update} />
+                <button onClick={this.Sbmit}>Submit</button>
+            </div>
+        )
+    }
+}
+
+export default AddPost
